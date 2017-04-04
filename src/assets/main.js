03a5d64a2d6f3d4eef75249bdf38bb29b0ebb6c1
majorazero <daniel.cw.hsu@gmail.com>
2017-04-03 20:22:02 -0700

    Added functionality to guess function.
    Added Attempt counter.
    Set condition to setHiddenFields().

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,8 +4,12 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+    if (attempt === 0 || answer === ''){
+      setHiddenFields();
+    }
 }
 function setHiddenFields (){
+  let attempt = 0;
   answer = Math.floor(Math.random()*9999);
   answer.toString();
   while(answer.length() < 4){
