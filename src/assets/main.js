6b6fbcf3f19a15e4b83612de3e564b5ec9093813
majorazero <daniel.cw.hsu@gmail.com>
2017-04-03 23:34:18 -0700

    Reinitialized some conditions.

--- src/assets/main.js
+++ src/assets/main.js
@@ -6,7 +6,7 @@ let results = document.getElementById('results');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
-    if (attempt === 0 || answer === ''){
+    if (attempt === '' || answer === ''){
       setHiddenFields();
     }
     if(!validateInput(input.value)){
