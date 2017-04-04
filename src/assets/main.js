620f814f5dcd20f01f67fb7fafe8dc7a5f15d36c
majorazero <daniel.cw.hsu@gmail.com>
2017-04-04 02:28:04 -0700

    ???
    Maybe I miswrote the win message. IDK.

--- src/assets/main.js
+++ src/assets/main.js
@@ -13,8 +13,8 @@ function guess() {
       return false;
     }
     attempt.value++;
-    if(getResults(input.value)===true){ //win condition
-      setMessage('You Win! :)');
+    if(getResults(input.value)){ //win condition
+      setMessage("You Win! :)");
       showAnswer(true);
       showReplay();
     }
