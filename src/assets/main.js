acc95dcb965706143c0aa6d6f119d05c19f962e3
majorazero <daniel.cw.hsu@gmail.com>
2017-04-04 18:42:57 -0700

    Changed 2 variables to actually do something.
    -_-

--- src/assets/main.js
+++ src/assets/main.js
@@ -6,7 +6,7 @@ let results = document.getElementById('results');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
-    if (attempt === '' || answer === ''){
+    if (attempt.value === '' || answer.value === ''){
       setHiddenFields();
     }
     if(!validateInput(input.value)){
